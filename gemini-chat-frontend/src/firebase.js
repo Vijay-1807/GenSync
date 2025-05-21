f2fa917695af9ccb81f3ea980350524e7053cae6
Bontha Vijay <bonthavijay1807@gmail.com>
2025-05-21 12:05:47 +0530

    Update firebase.js

--- gemini-chat-frontend/src/firebase.js
+++ gemini-chat-frontend/src/firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC-Te8BS3XxqN21QzH-pcPRwJsl2n3Pprc",
-    authDomain: "gemini-chat-app-87a18.firebaseapp.com",
-    projectId: "gemini-chat-app-87a18",
-    storageBucket: "gemini-chat-app-87a18.firebasestorage.app",
-    messagingSenderId: "149845948022",
-    appId: "1:149845948022:web:2ba0f7f2f8336022769157"
+    YOURS DETAILS
   };
 
 const app = initializeApp(firebaseConfig);
